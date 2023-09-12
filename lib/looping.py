a7124c95e1853e46506b4b4660a3bc91d48b7b1d
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-09-12 19:28:34 -0400

    passes three tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i >= 1:
+        print (i)
+        i-=1
+    print ("Happy New Year!")
+        
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squaring = [number * number for number in int_list]
+    return squaring
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if (i % 15 == 0):
+            print ("FizzBuzz")
+        elif (i %3 ==0):
+            print ("Fizz")
+        elif (i %5 == 0):
+            print ("Buzz")
+        else:
+            print (i)
+
+    
